e441e02a5d8ed8b1cb2bbffd59842522f6d56661
andrey104 <andreyrs104@gmail.com>
2018-02-26 00:25:04 +0300

    add send price to email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  email;
+  emailError = false;
+
+  constructor (private http: HttpClient){
+
+  }
+
+  emailForm = new FormGroup({
+    email: new FormControl(this.email, [
+      Validators.required,
+      Validators.email
+    ]),
+  });
+
+  ok() {
+    if (this.emailForm.valid) {
+      this.email = this.emailForm.get('email').value;
+      this.sendPriceRequest(this.email);
+      this.emailError = false;
+    } else {
+      this.emailError = true;
+    }
+  }
+
+
+  sendPriceRequest(email) {
+    this.http
+      .post('http://92.53.120.99/api/price/',{ "email": email})
+      .subscribe(
+        data => {
+          alert('Прайс успешно отправлен!')
+        },
+        err => {
+          alert('Неудалось отправить прайс. Попробуйте позже.')
+        }
+      );
+  }
 }
